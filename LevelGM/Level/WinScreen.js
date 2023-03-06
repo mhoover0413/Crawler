4e23bb7fa4a00d484af1c8d97712a69b4b4c2950
mhoover0413 <79530557+mhoover0413@users.noreply.github.com>
2023-03-06 11:11:23 -0500

    Bow Combat

--- LevelGM/Level/WinScreen.js
+++ LevelGM/Level/WinScreen.js
@@ -4,3 +4,10 @@ function nextLevel() {
     window.location.href = "https://brand24376.github.io/";
 }
 // End Next Level Button
+
+// Begin Level Selection Button
+function levelSelect() {
+    // Change the href and delete this comment when the next level is made
+    window.location.href = "../LevelSelection.html";
+}
+// End Level Selection Button
